19c7f62d9014c6f123348d5b305925454f6e4e06
daniel.nevels <daniel.nevels@DEN.coloradotech.edu>
2016-09-08 16:51:18 -0600

    Committed

--- test_deal_hand.py
+++ test_deal_hand.py
@@ -5,9 +5,7 @@ def test_deal_hand():
     assert(len(deal_hand()) == 5)
 	
 def test_pairs():
-	pair_hand = [Card(rank='4', suit='♣'), Card(rank='5', suit='♠'), Card(rank='5', suit='♦'), Card(rank='6', suit='♥'), Card(rank='7', suit='♥')
-	two_pair_hand = [Card(rank='4', suit='♣'), Card(rank='5', suit='♠'), Card(rank='5', suit='♦'), Card(rank='6', suit='♥'), Card(rank='6', suit='♦')
-    ranks = ['4', '5', '8', '6', '7']
+	ranks = ['4', '5', '8', '6', '7']
 	assert find_pairs(ranks) == 0
 	ranks = ['4', '5', '5', '6', '7']
 	assert find_pairs(ranks) == 1
@@ -15,25 +13,27 @@ def test_pairs():
 	assert find_pairs(ranks) == 2
 	
 def test_three_of_a_kind():
-	hand = [Card(rank='4', suit='♣'), Card(rank='4', suit='♠'), Card(rank='4', suit='♦'), Card(rank='6', suit='♥'), Card(rank='7', suit='♥')
-	assert three_of_a_kind(hand) == 1
+	ranks = ['4', '5', '5', '5', '6']
+	assert three_of_a_kind(ranks) == 1
 	
 def test_four_of_a_kind():
-	hand = [Card(rank='4', suit='♣'), Card(rank='4', suit='♠'), Card(rank='4', suit='♦'), Card(rank='4', suit='♥'), Card(rank='7', suit='♥')
-	assert four_of_a_kind(hand) == 1
+	ranks = ['4', '4', '4', '4', '5']
+	assert four_of_a_kind(ranks) == 1
 
 def test_straight():
-	hand = [Card(rank='4', suit='♣'), Card(rank='5', suit='♣'), Card(rank='6', suit='♣'), Card(rank='7', suit='♣'), Card(rank='8', suit='♣')
-	assert straight(hand) == 1
+	ranks = ['4', '5', '6', '7', '8']
+	assert straight(ranks) == 1
 	
 def test_flush():
-	hand = [Card(rank='4', suit='♣'), Card(rank='9', suit='♣'), Card(rank='6', suit='♣'), Card(rank='2', suit='♣'), Card(rank='8', suit='♣')
+	hand = [Card('4','♣'), Card('5','♣'), Card('9','♣'), Card('7','♣'), Card('10','♣')]
 	assert flush(hand) == 1
 
 def test_full_house():
-	hand = [Card(rank='4', suit='♣'), Card(rank='4', suit='♠'), Card(rank='4', suit='♦'), Card(rank='7', suit='♣'), Card(rank='7', suit='♥')
-	assert full_house(hand) == 1
+	hand = [Card('4', '♣'), Card('4', '♠'), Card('4', '♦'), Card('7', '♣'), Card('7', '♥')]
+	rank_list = get_ranks(hand)
+	assert full_house(rank_list) == 1
 
 def test_straight_flush():
-	hand = [Card(rank='4', suit='♣'), Card(rank='5', suit='♣'), Card(rank='6', suit='♣'), Card(rank='7', suit='♣'), Card(rank='8', suit='♣')
-	assert straight_flush(hand) == 1
+	hand = [Card('4', '♣'), Card('5', '♣'), Card('6', '♣'), Card('7', '♣'), Card('8', '♣')]
+	rank_list = get_ranks(hand)
+	assert straight_flush(hand, rank_list)
